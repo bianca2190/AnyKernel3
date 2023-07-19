9dd52c9b7820308ce4ff9373b133e9f72e57b63a
bianca2190 <109406227+bianca2190@users.noreply.github.com>
2023-07-20 06:42:58 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=RedDragon-KSU by MKV @mkvaja
+kernel.string=RedDragon-KSU by M03L.id @mkvaja
 do.devicecheck=1
 do.modules=0
 do.systemless=1
